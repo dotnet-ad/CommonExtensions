77bf28ecf21f44909b2833850c66b7552202c384
Ned Lanon <ned.lanon@outlook.com>
2016-02-01 14:11:12 +0100

    Adding datetime and reflection extensions.

--- CommonExtensions/CommonExtensions/DateTimeExtensions.cs
+++ CommonExtensions/CommonExtensions/DateTimeExtensions.cs
@@ -14,7 +14,7 @@ namespace CommonExtensions
     /// <summary>
     /// All extensions related to DateTime.
     /// </summary>
-    public class DateTimeExtensions
+    public static class DateTimeExtensions
     {
         #region Timestamps
 
@@ -37,6 +37,30 @@ namespace CommonExtensions
         {
             TimeSpan ts = (datetime.ToUniversalTime() - new DateTime(1970, 1, 1,0, 0, 0, DateTimeKind.Utc));
             return (long)ts.TotalMilliseconds;
+        }
+
+        #endregion
+
+        #region Comparing to Now
+        
+        /// <summary>
+        /// Elapseds time from now.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <returns>The elapsed timespan from now.</returns>
+        public static TimeSpan Elapsed(this DateTime datetime)
+        {
+            return DateTime.Now - datetime;
+        }
+
+        /// <summary>
+        /// Indicates whether the date is older than now.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <returns>True if the date is older than now.</returns>
+        public static bool HasPassed(this DateTime datetime)
+        {
+            return datetime.Elapsed() > TimeSpan.Zero;
         }
 
         #endregion
---
+++ CommonExtensions/CommonExtensions/ReflectionExtensions.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="ReflectionExtensions.cs">Copyright (c) 2015 All Rights Reserved </copyright>
+// <author>Aloïs Deniel</author>
+// <date>6/5/2015 3:49:31 PM</date>
+
+namespace CommonExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    /// <summary>
+    /// Extensions that use reflection.
+    /// </summary>
+    public static class ReflectionExtensions
+    {
+        #region Properties
+
+        /// <summary>
+        /// Determines whether the value is the name of one of the given properties.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name"></param>
+        /// <param name="properties"></param>
+        /// <returns></returns>
+        public static bool IsProperty<T>(this string name, params Expression<Func<T>>[] properties)
+        {
+            foreach (var property in properties)
+            {
+                if (name.IsProperty(property))
+                    return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Indicates whether the value is the name of the given property.
+        /// </summary>
+        /// <typeparam name="T">The type of the property</typeparam>
+        /// <param name="name">The tested string/param>
+        /// <param name="property">The property</param>
+        /// <returns>True of name is equals to the name of the property's name</returns>
+        public static bool IsProperty<T>(this string name, Expression<Func<T>> property)
+        {
+            var body = property.Body as MemberExpression;
+            var info = body.Member as PropertyInfo;
+            return name == info.Name;
+        }
+
+        /// <summary>
+        /// Gets the name of the property.
+        /// </summary>
+        /// <typeparam name="T">The type of the property</typeparam>
+        /// <param name="propertyExpression"></param>
+        /// <returns></returns>
+        public static string GetPropertyName<T>(this object owner, Expression<Func<T>> property)
+        {
+            return property.Name;
+        }
+
+        #endregion
+    }
+}
